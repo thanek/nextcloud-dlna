ee8af88b030bb33e94da4cb3bb33492208cd3456
xis <xis@schowek.net>
2023-10-08 10:46:54 +0200

    Initial commit

---
+++ src/main/kotlin/net/schowek/nextclouddlna/NextcloudDlnaApplication.kt
@@ -0,0 +1,11 @@
+package net.schowek.nextclouddlna
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NextcloudDlnaApplication
+
+fun main(args: Array<String>) {
+	runApplication<NextcloudDlnaApplication>(*args)
+}
---
+++ src/test/kotlin/net/schowek/nextclouddlna/NextcloudDlnaApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.schowek.nextclouddlna
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NextcloudDlnaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
