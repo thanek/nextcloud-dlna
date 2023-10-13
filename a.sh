6a142badf4d21bb5e7b8b31c28343c2b98ab0953
xis <xis@schowek.net>
2023-10-13 20:33:16 +0200

    cleanup

--- a.sh
+++
@@ -1,4 +0,0 @@
-while :; do
-    echo  "hello"
-    sleep 1
-done
--- run.sh
+++
@@ -1,9 +0,0 @@
-docker run -d \
-  --name="nextcloud-dlna" \
-  --net=host \
-  -e NEXTCLOUD_DLNA_INTERFACE=en0 \
-  -e NEXTCLOUD_DB_HOST='192.168.0.88' \
-  -e NEXTCLOUD_DB_PASS='p4szt3t!' \
-  -v '/Users/xis/projects/playground/docker/nextcloud/app/data:/nextcloud' \
-  -e NEXTCLOUD_DATA_DIR=/nextcloud \
-  nextcloud-dlna
