b8a9c451d404a74043633875c1b8db68952b9785
xis <xis@schowek.net>
2023-10-31 17:43:01 +0100

    docker folder

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-version=`./gradlew currentVersion | grep "Project version" | awk -F"version: " '{ print $2 }'`
+dir=$(realpath "$(dirname "$0")")
+
+cd "$dir/../" || exit
+
+version=$(./gradlew currentVersion | grep "Project version" | awk -F"version: " '{ print $2 }')
 
 echo "This will create docker image for version $version."
 read -p "Continue [y/n]? " -n 1 -r
@@ -21,9 +25,13 @@ docker buildx inspect --bootstrap
 docker buildx build \
   --push \
   --platform=linux/arm/v7,linux/arm64/v8,linux/amd64 \
+  --file=./docker/Dockerfile \
   --tag=thanek/nextcloud-dlna:$version .
 
 docker buildx build \
   --push \
   --platform=linux/arm/v7,linux/arm64/v8,linux/amd64 \
+  --file=./docker/Dockerfile \
   --tag=thanek/nextcloud-dlna .
+
+cd - || exit
